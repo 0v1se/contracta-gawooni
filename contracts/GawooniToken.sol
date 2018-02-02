27025f8cd33b269aa37cbd604ddcd9fe5936bd6d
0v1se <dev@0v1se.com>
2018-02-02 17:44:30 +0300

    Token implemented

---
+++ contracts/GawooniToken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.0;
+
+import "@daonomic/tokens/contracts/BurnableTokenImpl.sol";
+import "@daonomic/tokens/contracts/MintableTokenImpl.sol";
+import "@daonomic/tokens/contracts/PausableToken.sol";
+import "@daonomic/util/contracts/OwnableImpl.sol";
+
+contract GawooniToken is OwnableImpl, PausableToken, MintableTokenImpl, BurnableTokenImpl {
+	string public constant name = "GAWOONI";
+	string public constant symbol = "GWON";
+	uint8 public constant decimals = 18;
+
+	function burn(uint256 _value) public whenNotPaused {
+		super.burn(_value);
+	}
+}
