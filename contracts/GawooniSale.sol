3445c68943be233c574638695c253586e6f16ccb
0v1se <dev@0v1se.com>
2018-02-24 16:01:48 +0300

    replaced some old var names

--- contracts/GawooniSale.sol
+++ contracts/GawooniSale.sol
@@ -8,7 +8,7 @@ import "@daonomic/sale/contracts/PeriodSale.sol";
 contract GawooniSale is OwnableImpl, MintingSale, CappedBonusSale, PeriodSale {
 	address public btcToken;
 	uint256 public btcEthRate = 10 * 10**10;
-	uint256 public constant ethEt4Rate = 1000 * 10**18;
+	uint256 public constant ethRate = 1000 * 10**18;
 
 	function GawooniSale(
 		address _mintableToken,
@@ -26,9 +26,9 @@ contract GawooniSale is OwnableImpl, MintingSale, CappedBonusSale, PeriodSale {
 
 	function getRate(address _token) constant public returns (uint256) {
 		if (_token == btcToken) {
-			return btcEthRate * ethEt4Rate;
+			return btcEthRate * ethRate;
 		} else if (_token == address(0)) {
-			return ethEt4Rate;
+			return ethRate;
 		} else {
 			return 0;
 		}
